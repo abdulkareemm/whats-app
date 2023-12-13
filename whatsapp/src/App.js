9707243397bf16d7b53ac9bc81894a16f70ca872
abdulkareemm <eng.kreem.96@gmail.com>
2023-12-13 00:17:36 -0800

    setup routes frontend

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,6 +1,18 @@
 import "./App.css";
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+import {Login,Home,Register} from "./pages"
 function App() {
-  return <div className="dark">asd</div>;
+  return (
+    <div className="dark">
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
