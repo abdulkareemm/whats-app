c34095f3e62b4dad97f6829f460b72cf25b40d11
abdulkareemm <eng.kreem.96@gmail.com>
2023-12-22 02:25:33 -0800

    sidebar notification

--- whatsapp/src/components/sidebar/Sidebar.jsx
+++ whatsapp/src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import SidebarHeader from './SidebarHeader'
+import Notification from './Notification';
 
 const Sidebar = () => {
   return (
     <div className="flex0030 max-w-[30%] h-full select-none">
       {/* header */}
       <SidebarHeader />
-      
+      {/* notifications */}
+      <Notification/>
     </div>
-  )
+  );
 }
 
 export default Sidebar
