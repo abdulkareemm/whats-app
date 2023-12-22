d95a1924db798177b842efc120b4b055847e05cd
abdulkareemm <eng.kreem.96@gmail.com>
2023-12-22 02:21:10 -0800

    sidebar frontend

--- whatsapp/src/components/index.js
+++ whatsapp/src/components/index.js
@@ -1,2 +1,3 @@
-export { default as RegisterForm} from "./auth/RegisterForm"
+export { default as RegisterForm } from "./auth/RegisterForm";
 export { default as LoginForm } from "./auth/LoginForm";
+export { default as Sidebar } from "./sidebar/Sidebar";
---
+++ whatsapp/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SidebarHeader from './SidebarHeader'
+
+const Sidebar = () => {
+  return (
+    <div className="flex0030 max-w-[30%] h-full select-none">
+      {/* header */}
+      <SidebarHeader />
+      
+    </div>
+  )
+}
+
+export default Sidebar
